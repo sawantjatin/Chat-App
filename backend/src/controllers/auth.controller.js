71da93a2dbf374e7c3d16ed54fa540bbf82a1327
Jatin Suhas Sawant <jatinsawant02@gmail.com>
2026-07-09 20:25:24 +0530

    Add user signup authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,63 @@
-export const signup = (req, res) => {
-  res.send("Signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    //hash password
+    // Password length check
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    // Check if user already exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        message: "User created successfully",
+        user: {
+          id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          profilePic: newUser.profilePic,
+        },
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid user data",
+      });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error.message);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
 export const login = (req, res) => {
