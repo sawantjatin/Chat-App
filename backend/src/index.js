752f71688a5a353f581409d7c7c129f1cb4b4384
Jatin Suhas Sawant <jatinsawant02@gmail.com>
2026-07-10 22:38:06 +0530

    Add user login and logout Auth without middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middlewares/auth.middleware.js";
 import authRoutes from "./routes/auth.route.js";
-import connectDB from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -10,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,4 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {};
